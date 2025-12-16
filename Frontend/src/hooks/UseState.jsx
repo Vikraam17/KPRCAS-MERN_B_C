e786d77d6da0f20dcda37f34c5ea7d1c1bb074e4
Viky <vikyvino1773@gmail.com>
2025-12-16 15:36:38 +0530

    Express Created

--- Frontend/src/hooks/UseState.jsx
+++ Frontend/src/hooks/UseState.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 const UseState = () => {
-    //const [value,setValue] =useState(initialValue);
+    // const [value,setValue] =useState(initialValue);
     const [count,setCount]=useState(0);
     const [value,setValue] =useState("");
     const handleDecrement = ()=>{
@@ -12,10 +12,10 @@ const UseState = () => {
     <div>
       <h1>{value}</h1>
       <input type="text" onChange={(e)=>setValue(e.target.value)} placeholder='Enter the value'/>
-        {/* <h1>{count}</h1>
+        <h1>{count}</h1>
         <button onClick={handleDecrement}>Decrement</button>
         <button onClick={()=>setCount(0)}>Reset</button>
-        <button onClick={()=>setCount(count+1)}>Increment</button> */}
+        <button onClick={()=>setCount(count+1)}>Increment</button> 
     </div>
   )
 }
